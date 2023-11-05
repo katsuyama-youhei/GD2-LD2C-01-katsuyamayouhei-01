5eabc8a8f1c02a2d154854bec27df91dc958f8f1
katsuyama-youhei <K022G0044@g.neec.ac.jp>
2023-11-05 23:25:55 +0900

    タイトルシーンのスペースを上下移動させるアニメーションの追加

--- Assets/TitleProto/FadeSceneScript.cs
+++ Assets/TitleProto/FadeSceneScript.cs
@@ -2,36 +2,23 @@ using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 
-public class ClearSprite : MonoBehaviour
+public class FadeSceneScript : MonoBehaviour
 {
-
-    public GameObject clearText;
-
-    public GorlScript gorlScript;
-
     [SerializeField] private string loadScene;
     [SerializeField] private Color fadeColor = Color.black;
     [SerializeField] private float fadeSpeedMultiplier = 1.0f;
     // Start is called before the first frame update
     void Start()
     {
-       
-     
+        
     }
 
     // Update is called once per frame
     void Update()
     {
-        if (gorlScript.isClea)
+        if (Input.GetKeyDown(KeyCode.Space))
         {
-            clearText.SetActive(true);
-
-            if (Input.GetKeyDown(KeyCode.Space))
-            {
-                Initiate.Fade(loadScene, fadeColor, fadeSpeedMultiplier);
-            }
+            Initiate.Fade(loadScene, fadeColor, fadeSpeedMultiplier);
         }
     }
-
-   
 }
